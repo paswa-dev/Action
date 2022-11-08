ba938a84eeef7fc178a122c981c6fba21de33006
paswa-dev <73659592+paswa-dev@users.noreply.github.com>
2022-11-08 10:27:05 -0500

    New Change

---
+++ ClassService.lua
@@ -0,0 +1,59 @@
+
+
+local replicated = game:GetService("ReplicatedStorage")
+local module = {}
+module.__index = module
+
+local function table_contains(t, v)
+    for i, value in ipairs(t) do
+        if value == v then
+            return i
+        end
+    end
+    return nil
+end
+
+function module.new(class, sub_folder)
+    sub_folder = sub_folder or replicated
+    local control_pickup = Instance.new("RemoteEvent", sub_folder)
+    ---
+    local config_tb = {}
+    config_tb.Subscribed = {}
+    config_tb.OnPlayerRemove = Instance.new("BindableEvent")
+    config_tb.OnPlayerAdd = Instance.new("BindableEvent")
+    config_tb.Binded = {}
+
+    control_pickup.OnServerEvent:Connect(function(player, key_pickup)
+        if table_contains(config_tb.Subscribed, player) then
+            if config_tb.Binded[key.KeyCode] then
+                config_tb.Binded[key.KeyCode](player)
+            elseif config_tb.Binded[key.UserInputType] then
+                config_tb.Binded[key.UserInputType](player)
+            end
+        end
+    end)
+
+    return setmetatable(config_tb, module)
+end
+
+function module:subscribe(player)
+    if table_contains(self.Subscribed, player) then
+        return debug.traceback("Player already subscribed.")
+    end
+    table.insert(self.Subscribed, player)
+    self.OnPlayerAdd.Fire(player)
+end
+
+function module:unsubscribe(player)
+    local success, err = pcall(function()
+        table.remove(self.Subscribed, table_contains(self.Subscribed, player))
+    end)
+
+    if not success then
+        return debug.traceback("Error occured when attempting to unsubscribe user.")
+    elseif success then
+        self.OnPlayerRemove.Fire(player)
+    end
+end
+
+return module
---
+++ StateService.lua
@@ -0,0 +1,82 @@
+local module = {}
+module.__index = module
+
+local function table_contains(t, v)
+    for i, value in ipairs(t) do
+        if value == v then
+            return i
+        end
+    end
+    return nil
+end
+
+function module.new(object, states : {any})
+    states = states or {}
+
+    local config_lb = {}
+    config_lb.Object = object
+    config_lb.States = states
+    config_lb.OnStateChange = Instance.new("BindableEvent")
+    config_lb.OnObjectChange = Instance.new("BindableEvent")
+
+    return setmetatable(config_lb, module)
+end
+
+function module:Remove()
+    self = nil
+end
+
+function module:Clear()
+    self.States = {}
+end
+
+function module:SetObject(object)
+    self.Object = object
+end
+
+function module:AddState(state)
+    if table_contains(self.States, state) then
+        return debug.traceback("State already exists.")
+    else
+        self.OnStateChange.Fire(self.Object, state, "A")
+        table.insert(self.States, state)
+    end
+end
+
+
+function module:RemoveState(state)
+    local success, err = pcall(function()
+        table.remove(self.States, table_contains(self.States, state))
+    end)
+
+    if not success then
+        return debug.traceback("Error when attempting to remove non-existing state.")
+    elseif success then
+        self.OnStateChange.Fire(self.Object, state, "R")
+    end
+end
+
+function module:AppendStates(states: {any})
+    for _, state in ipairs(states) do
+        if table_contains(self.States, state) then
+            debug.traceback("State already exists. Skipping...")
+        else
+            self.OnStateChange.Fire(self.Object, state, "A")
+            table.insert(self.States, state)
+        end
+    end
+end
+
+function module:RemoveStates(states: {any})
+    for _, state in ipairs(states) do
+        local success, err = pcall(function()
+            table.remove(self.States, table_contains(self.States, state))
+        end)
+    
+        if not success then
+            debug.traceback("Error when attempting to remove non-existing state.")
+        elseif success then
+            self.OnStateChange.Fire(self.Object, state, nil)
+        end
+    end
+end
