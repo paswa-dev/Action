d806c8b7b4e565ba1cdaa7636ce63f15692aaeee
paswa-dev <73659592+paswa-dev@users.noreply.github.com>
2022-11-14 09:31:53 -0500

    Wave

--- Utilities/WaveGenerator.lua
+++ Utilities/WaveGenerator.lua
@@ -1,17 +1,23 @@
 local module = {}
 
 local e = 2.71828182846 -- Natural Log (Should i add?)
-local g = 9.81
-local pi = math.pi
+local g = 9.81 -- gravity
+local pi = math.pi -- Pi
+
+-- Formula for reference
+-- https://wikimedia.org/api/rest_v1/media/math/render/svg/6a1f2a92d197cce47f77a919abe207d61bc57c92
+
+
 
 function module.wave(position, wave_length, steepness, direction_2d, _time, time_constant)
     time_constant = time_constant or 1
-    local k = ( 2 * pi ) / wave_length
-    local k_steep = steepness/k
+    local k = ( 2 * pi ) / wave_length -- Wave Number (Non linear)
+    local k_steep = steepness/k -- Steepness
     local d = direction_2d.Unit -- X/Z -> X,Y
-    local c = math.sqrt(g/k)
+    local c = math.sqrt(g/k) -- Phase Speed
     local f = k * (d:Dot(Vector2.new(position.X, position.Y)) - c * (_time/time_constant))
-    --        wave_number * (angle between direction and point) - speed * time
+    --        wave_number * (angle between direction and point) - phase_speed * time
+    
     local f_cos = math.cos(f)
 
     local x = d.X * (k_steep * f_cos)
