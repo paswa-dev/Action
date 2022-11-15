d7e8261b553282ab0f97463f8d909b6828c41f95
paswa-dev <73659592+paswa-dev@users.noreply.github.com>
2022-11-15 09:37:21 -0500

    Misc

--- Utilities/PromptService.lua
+++ Utilities/PromptService.lua
@@ -12,22 +12,24 @@ end
 
 task.spawn(function()
     while task.wait() do
-        if not object then return
+        if not object then return end
         for i, v in next, prompt.storage then
-            local index = table.find(prompt.activated, i)
-            local d = get_distance(prompt.object.Position, i.Position)
-            if ( d >= v.radius) and not index then
-                table.insert(prompt.activated, i)
-                v.enter()
-            elseif ( d < v.radius) and index then
-                table.remove(prompt.activated, index)
-                v.leave()
-            end
+            pcall(function()
+                local index = table.find(prompt.activated, i)
+                local d = get_distance(prompt.object.Position, i.Position)
+                if ( d >= v.radius) and not index then
+                    table.insert(prompt.activated, i)
+                    v.enter()
+                elseif ( d < v.radius) and index then
+                    table.remove(prompt.activated, index)
+                    v.leave()
+                end
+            end)
         end
     end
 end)
 
-function prompt.new(adornee, enter_callback, leave_callback, radius)
+function prompt.new(adornee, radius, enter_callback, leave_callback)
     prompt.storage[adornee] = {enter=enter_callback, leave=leave_callback, radius=radius}
 end
 
