cb53a01f9e68e3b917a38f9fe63c7374f975561c
paswa-dev <73659592+paswa-dev@users.noreply.github.com>
2023-01-19 13:01:15 -0500

    VRWrapper Support

--- Utilities/VRWrapper.lua
+++ Utilities/VRWrapper.lua
@@ -1,4 +1,24 @@
-local VRWrap = {}
+local VR = game:GetService("VRService")
 
+local VRWrap = {}
+VRWrap.ACTIVE_CONTROLLERS = {}
+VRWrap.__index = VRWrap
+
+-- VRService
+
+function VRWrap.VRController(player, model)
+    if not VR.VREnabled then
+        debug.traceback("Player does not support a VRController")
+        return
+    end
+
+    local self = {} 
+    self.model = model
+    self.controller_host = player
+    self.CFrame_changed = 
+
+    table.insert(VRWrap.ACTIVE_CONTROLLERS, self)
+    return setmetatable(self, VRWrap)
+end
 
 return VRWrap
