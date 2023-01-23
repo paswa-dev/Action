63d7dcb3ffc214d2291e143e35e9949ddc13a7de
paswa-dev <73659592+paswa-dev@users.noreply.github.com>
2023-01-23 12:57:16 -0500

    Misc

--- Plugins/Git.lua
+++ Plugins/Git.lua
@@ -8,26 +8,15 @@ local commit_button = toolbar:CreateButton("Commit", "Commit")
 local connect_button = toolbar:CreateButton("Connect", "Connect")
 
 local github_api = "https://api.github.com"
-local complete_address = ""
 
 local history = {}
 local settings = {
-    github_token = ,
+    github_token = "",
     username = game.Players.LocalPlayer.Name .. "/" ..game.Players.LocalPlayer.UserId,
     github_name = ""
 }
 
-
-
-config_button.Click:Connect(function()
-    -- Show UI and settings
-end)
-
-commit_button.Click:Connect(function()
-    --Commit to the github address
-end)
-
-connect_button.Click:Connect(function()
+local function get(endpoint)
     if not settings.github_name or not settings.github_token then return end
     local payload = {
         ["name"]=settings.github_name
@@ -37,5 +26,14 @@ connect_button.Click:Connect(function()
         Accept = "application/vnd.github.v3+json"
     }
 
-    http:PostAsync(github_api .. "/user/repos", http:JSONEncode(payload), Enum.HttpContentType.ApplicationJson, false, headers)
+    local response = http:PostAsync(github_api .. endpoint, http:JSONEncode(payload), Enum.HttpContentType.ApplicationJson, false, headers)
+    return response
+end
+
+config_button.Click:Connect(function()
+    -- Show UI and settings
+end)
+
+commit_button.Click:Connect(function()
+    --Commit to the github address
 end)
