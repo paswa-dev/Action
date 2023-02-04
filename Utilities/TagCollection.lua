0e459928d741a528bbd309f0a9ced5cdfd76081f
paswa-dev <73659592+paswa-dev@users.noreply.github.com>
2023-02-03 20:29:51 -0500

    Update TagCollection.lua

--- Utilities/TagCollection.lua
+++ Utilities/TagCollection.lua
@@ -6,38 +6,38 @@ module.actions = {}
 
 
 local function runTag(tag, func)
-    if not func then return end
-    local tagged = Tag:GetTagged(tag)
-    if not tagged then table.remove(tagged); return end
-    for _, object in next, Tag:GetTagged(tag) do
-        func(object)
-    end
+	if not func then return end
+	local tagged = Tag:GetTagged(tag)
+	if not tagged then table.remove(tagged); return end
+	for _, object in next, Tag:GetTagged(tag) do
+		func(object)
+	end
 end
 
-local function init()
-    for _, tag in next, tag:GetAllTags() do
-        Tag:GetInstanceAddedSignal(tag, function() module.actions[tag].start end)
-        Tag:GetInstanceRemovedSignal(tag, function() module.actions[tag].end end)
-        if module.actions[tag] then runTag(tag, module.actions[tag].start) end
-    end)
-    init = true
+local function __init__()
+	for _, tag in next, Tag:GetAllTags() do
+		Tag:GetInstanceAddedSignal(tag):Connect(function(obj) module.actions[tag].START(obj) end)
+		Tag:GetInstanceRemovedSignal(tag):Connect(function(obj) module.actions[tag].END(obj) end)
+		if module.actions[tag] then runTag(tag, module.actions[tag].START) end
+	end
+	init = true
 end
 
 function module:Created(tag, func)
-    if init then
-        runTag(tag, func)
-    else
-        if not module.actions[tag] then module.actions[tag] = {} end
-        module.actions[tag].start = func
-    end
+	if init then
+		runTag(tag, func)
+	else
+		if not module.actions[tag] then module.actions[tag] = {} end
+		module.actions[tag].START = func
+	end
 end
 
 function module:Removed(tag, func)
-    if not module.actions[tag] then module.actions[tag] = {} end
-    module.actions[tag].end = func
-    -- When is removed do this
+	if not module.actions[tag] then module.actions[tag] = {} end
+	module.actions[tag].END = func
+-- When is removed do this
 end
 
-init()
+__init__()
 
 return module
